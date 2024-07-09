34f241bf9785314b3d658d623f5a361181befe50
Karla038 <karladk038@gmail.com>
2024-07-09 17:00:15 -0600

    Diseño del formulario de registro

--- TaskCute/src/app/app-routing.module.ts
+++ TaskCute/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { RegisterComponent } from './auth/register/register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthRoutingModule } from './auth/auth.routing'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent },
+  {path: '', pathMatch : 'full', redirectTo: 'users/register'},
+  {path: '**', pathMatch : 'full', redirectTo: 'users/inicio'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    AuthRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- TaskCute/src/app/auth/register/register.component.ts
+++ TaskCute/src/app/auth/register/register.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+    ruta:string = '/assets/images/logo.png'
 }
