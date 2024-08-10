946cdcdca33c0676e3d4c0b4ed9b6fda6831d4c4
Karla038 <124712008+Karla038@users.noreply.github.com>
2024-08-10 16:48:06 -0600

    Update app-routing.module.ts

--- TaskCute/src/app/app-routing.module.ts
+++ TaskCute/src/app/app-routing.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthRoutingModule } from './auth/auth.routing'
-import { TasckComponent } from './tasck/tasck.component';
-import { TaskTabComponent } from './task-tab/task-tab.component';
+import { TaskRoutingRoutesModule } from './tasck/task-routing.module';
 
 
 const routes: Routes = [
-  { path: 'tareas', component: TasckComponent },
-  {path: 'list-tareas', component:TaskTabComponent},
   {path: '', pathMatch : 'full', redirectTo: 'users/register'},
   {path: '**', pathMatch : 'full', redirectTo: 'users/inicio'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
-    AuthRoutingModule
+    AuthRoutingModule,
+    TaskRoutingRoutesModule
   ],
   exports: [RouterModule]
 })
